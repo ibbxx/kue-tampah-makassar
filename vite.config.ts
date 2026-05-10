7a630187b67c5d6cc7b08fb3b3d4084fa6644308
ibbxx <134438301+ibbxx@users.noreply.github.com>
2026-05-11 01:56:16 +0800

    chore: menambahkan plugin nitro dan menyesuaikan konfigurasi vite untuk integrasi Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
 
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
-    tailwindcss(),
     tanstackStart(),
+    nitro(),
+    tailwindcss(),
     react(),
   ],
   resolve: {
     tsconfigPaths: true,
-  },
-  optimizeDeps: {
-    exclude: [
-      "@tanstack/react-start",
-      "@tanstack/react-router",
-      "@tanstack/start-server-core",
-      "@tanstack/start-client-core",
-      "@tanstack/react-start-server",
-      "@tanstack/react-start-client",
-    ],
   },
 });
